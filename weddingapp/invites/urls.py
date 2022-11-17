673c939b71802bc401657dd0534fc128afe2cd43
benedictazucena <benjoazucena@gmail.com>
2022-11-17 17:46:52 +0800

    updated landing page

--- weddingapp/invites/urls.py
+++ weddingapp/invites/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('rsvp', views.rsvp, name='rsvp'),
     path('<invite_token>', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('gift', views.gift, name='gift'),
 
 ]
