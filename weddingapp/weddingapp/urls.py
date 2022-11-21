32865e654b758791333a9b33a1a1b13848673c1f
benedictazucena <benjoazucena@gmail.com>
2022-11-21 22:35:52 +0800

    redirect root to invite app

--- weddingapp/weddingapp/urls.py
+++ weddingapp/weddingapp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/invite/')),
     path('invite/', include('invites.urls')),
     path('admin/', admin.site.urls),
 ]
